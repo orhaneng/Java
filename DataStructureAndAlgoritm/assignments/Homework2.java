73592fefe448f8c185a33ce32054c90502ec4dcb
Omer Orhan <omer.orhan@lexinsight.ai>
2019-02-04 20:35:35 -0800

    HW

--- DataStructureAndAlgoritm/assignments/Homework2.java
+++ DataStructureAndAlgoritm/assignments/Homework2.java
@@ -1,8 +1,18 @@
-
-//OMER ORHAN
-
 import java.util.ArrayList;
 
+/*
+ * X419-01: Take-Home Assignment 1
+Assume you are given a function bool hit(int x, int y) that returns true 
+if there was a hit to a ship and false if not. You are to write a function 
+that returns the coordinates of a single ship on a grid of given size, the 
+function takes at least one argument int gridSize and must return the 
+coordinates of the 3 unit ship present in the board in the shortest 
+time possible. Estimate the Time and memory complexity of your proposed 
+solution. You are to select the data structure to return what you need as 
+output and the prototype of the function.
+ * 
+ * 
+ */
 public class Homework2 {
 
 	public static void main(String[] args) {
