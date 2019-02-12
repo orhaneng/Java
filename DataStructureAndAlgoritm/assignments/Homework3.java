d6545ea536b6b909736bc2d6935681cc962560a5
Omer Orhan <omer.orhan@lexinsight.ai>
2019-02-12 15:43:34 -0800

    cc

--- DataStructureAndAlgoritm/assignments/Homework3.java
+++ DataStructureAndAlgoritm/assignments/Homework3.java
@@ -2,6 +2,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
+
 /*
  * X419-01: Homework 3
 	1.-Reverse a queue using recursion.
@@ -17,16 +18,9 @@ public class Homework3 {
 		queue.add(3);
 		queue.add(9);
 		queue.add(1);
-		//print(queue);
-		//reverse(queue);
-		//print(queuenew);
 		
-		final String text = "public static void print(Queue<Integer> queue) {	\n" + 
-				"		for(Integer s : queue) { \n" + 
-				"			  System.out.println(s.toString()); \n" + 
-				"			}\n" + 
-				"	}";
-		System.out.println(checkparenthesis(text));
+		final String text = "{<}>";
+		System.out.println("checkparenthesis:"+checkparenthesis(text));
 
 	}
 
@@ -35,10 +29,14 @@ public class Homework3 {
 			  System.out.println(s.toString()); 
 			}
 	}
+	
+	
 	public static Queue<Integer> reverse(Queue<Integer> queue){
+		//If it is end of the queue, return queue
 		if(queue.size()==0) {
 			return queue;
 		}
+		//remove element and go to recursive
 		Integer value = queue.remove();
 		queue = reverse(queue);
 		queuenew.add(value);
@@ -49,14 +47,42 @@ public class Homework3 {
 		
 		Stack<String> stack = new Stack<String>();
 		for (int i = 0; i < text.length(); i++) {
-			if(text.charAt(i)=='{') {
+			if(text.charAt(i)=='{'||text.charAt(i)=='<'||text.charAt(i)=='['
+					||text.charAt(i)=='(') {
 				stack.add(Character.toString(text.charAt(i)));
 			}
-			else if(text.charAt(i)=='}'){
+			//if it is closing symbols, check with the correct one
+			// if there is not match, return false
+			else if(text.charAt(i)=='}'||text.charAt(i)=='>'||text.charAt(i)==']'
+					||text.charAt(i)==')'){
+				
+				if(text.charAt(i)=='}') {
+					if((int)stack.peek().charAt(0)!= (int)"{".charAt(0))
+					{
+						return false;
+					}	
+				}
+				if(text.charAt(i)=='>') {
+					if((int)stack.peek().charAt(0)!= (int)"<".charAt(0))
+					{
+						return false;
+					}	
+				}
+				if(text.charAt(i)==']') {
+					if((int)stack.peek().charAt(0)!= (int)"[".charAt(0))					{
+						return false;
+					}	
+				}
+				if(text.charAt(i)==')') {
+					if((int)stack.peek().charAt(0)!= (int)"(".charAt(0))					{
+						return false;
+					}	
+				}
+				
 				stack.pop();
 			}
 		}
-		
+		//if stack is not empty, return false
 		if(stack.size()!=0) return false;
 		else return true;
 	}
