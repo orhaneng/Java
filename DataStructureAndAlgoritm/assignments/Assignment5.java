e385641534743c42fed1faf2f76c48bb48460fbd
Omer Orhan <omer.orhan@lexinsight.ai>
2019-03-09 14:22:41 -0800

    CC

---
+++ DataStructureAndAlgoritm/assignments/Assignment5.java
@@ -0,0 +1,35 @@
+/*
+ * -Create a program for simulating the fortune unix command. 
+ * -The file that contains fortunes uses % in a single line to separate fortune statements. 
+ * -The fortune command mist parse the file and represent in memory and then select at 
+ * random one statement and print that every time invoked.
+ * -We assume that the contents of the file fit in memory properly and is not too big.
+   -If empty statement in the fortune file is empty then if that statement is selected at 
+random, an empty string must be printed.
+   -Statements may be multiline and they should be printed verbatim if that is the case.
+As an example:
+Your fortune is bad today!
+%
+No Fortune Today! Sorry.
+%
+Today is a good day...
+To play the lottery
+%
+%
+Sorry not much fortune today!
+%
+There are 5 statements above so your program must parse the file and find that out and 
+store the string in some data structure then select a number between 0 and 4 if 2 is 
+selected the following must be printed:
+Today is a good day...
+
+To play the lottery
+ */
+public class Assignment5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ DataStructureAndAlgoritm/assignments/Homework6.java
@@ -0,0 +1,45 @@
+/*
+ * Create a recursive function that takes a string of digits and counts the possible 
+ * ways in which the string can be partition into a sequence of even numbers:
+input 10 ==> only one way to partition into even numbers 10
+input 22 ==> 2 2 
+                     22 
+result is 2
+input 333 => result is 0
+
+TEST CASES
+-DIGIT=22
+ COUNT=2
+-DIGIT=1234
+ EVEN NUMBERS:2,4,12,34,234
+ COUNT=5  
+TIME COMPLEXITY:
+ */
+public class Homework6 {
+
+	public static void main(String[] args) {
+		System.out.println(divideDigit("1234", 0, 1, 0));
+	}
+
+	public static int divideDigit(String digits, int start_index, int lenght, int count) {
+
+		if (lenght == digits.length()) {
+			return count;
+		}
+		// CHECK EVEN FOR EVERY ELEMENT
+		int number = Integer.parseInt(digits.substring(start_index, start_index + lenght));
+		if (number % 2 == 0) {// IF NUMBER IS EVEN ADD COUNT
+			count++;
+		}
+		if (start_index + lenght == digits.length()) {
+			// IF INDEX IS END OF THE DIGIT, INCREASE LENGHT AND CONTROL LARGER STRING
+			lenght = lenght + 1;
+			start_index = 0;
+		} else {
+			// CONTROL EACH STRING
+			start_index = start_index + 1;
+		}
+		return divideDigit(digits, start_index, lenght, count);
+	}
+
+}
