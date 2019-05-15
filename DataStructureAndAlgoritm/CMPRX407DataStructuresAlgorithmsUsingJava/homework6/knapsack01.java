aa5ac15f18eac8294d97b293564f380ddf515ca4
Omer Orhan <omer.orhan@lexinsight.ai>
2019-05-15 00:19:17 -0700

    cc

--- DataStructureAndAlgoritm/CMPRX407DataStructuresAlgorithmsUsingJava/homework6/knapsack01.java
+++ DataStructureAndAlgoritm/CMPRX407DataStructuresAlgorithmsUsingJava/homework6/knapsack01.java
@@ -1,5 +1,7 @@
 package homework6;
 
+import java.util.ArrayList;
+
 /**
  * File Name: knapsack01.java Dynamic programming
  * 
@@ -12,6 +14,10 @@ class knapsack01 extends knapsack01Base {
 	private int[] wa; // weight array: Given
 	private int[] ca; // cost array: Given
 	private int[] maxans; // Final value of items stolen. You need to compute and fill this.
+	int[][] vmatrix;
+	int[][] kmatrix;
+	ArrayList<Integer> item;
+	ArrayList<Integer> value;
 
 	// You can use as many private data structure you want
 
@@ -22,8 +28,10 @@ class knapsack01 extends knapsack01Base {
 
 	@Override
 	protected void knapsack(int bagSize, int[] weightArray, int[] costArray, int[] maxans) {
-		int[][] vmatrix = new int[weightArray.length + 1][bagSize + 1];
-		int[][] kmatrix = new int[weightArray.length + 1][bagSize + 1];
+		wa = weightArray;
+		ca = costArray;
+		vmatrix = new int[weightArray.length + 1][bagSize + 1];
+		kmatrix = new int[weightArray.length + 1][bagSize + 1];
 
 		for (int i = 0; i <= weightArray.length; i++) {
 			for (int j = 0; j <= bagSize; j++) {
@@ -74,7 +82,44 @@ class knapsack01 extends knapsack01Base {
 			System.out.print(costArray[i] + " ");
 		}
 		System.out.println();
+		System.out.print("Max Value of " + maxans[0] + " can obtained from items {");
+		item = new ArrayList<Integer>();
+		value = new ArrayList<Integer>();
+		findPath(maxans[0], 0);
+		for (int i = 0; i < item.size(); i++) {
+			if (i == item.size() - 1)
+				System.out.print(item.get(i));
+			else
+				System.out.print(item.get(i) + ",");
+		}
 
+		System.out.print("} that has values {");
+		for (int i = 0; i < value.size(); i++) {
+			if (i == item.size() - 1)
+				System.out.print(value.get(i));
+			else
+				System.out.print(value.get(i) + "+");
+		}
+		System.out.println("=" + maxans[0] + "}");
+	}
+
+	private void findPath(int max, int index) {
+		if (max == 0)
+			return;
+		for (int i = 0; i < vmatrix.length; i++) {
+			for (int j = 0; j < vmatrix[0].length; j++) {
+				if (max == vmatrix[i][j]) {
+					max = max - ca[i - 1];
+					item.add(i);
+					value.add(ca[i - 1]);
+					findPath(max, index + 1);
+					i = vmatrix.length;
+					break;
+				}
+
+			}
+
+		}
 	}
 
 	public static void main(String[] args) {
