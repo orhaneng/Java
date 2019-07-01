534310c69f29aa8c18ce15884e3026c8313e95b3
omer <omer@VAIO>
2019-06-30 20:51:08 -0700

    cc

---
+++ advanced-jaxrs-01/src/main/java/org/orhan/javabrains/org/orhan/javabrains/client/RestApiClient.java
@@ -0,0 +1,10 @@
+package org.orhan.javabrains.client;
+
+public class RestApiClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("deneme");
+	}
+
+}
