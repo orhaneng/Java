f24e18dd5dc4cdf41339b70b45e8f8853875fddc
omer <omer@VAIO>
2019-06-30 21:26:44 -0700

    cc

--- advanced-jaxrs-01/src/main/java/org/orhan/javabrains/org/orhan/javabrains/client/RestApiClient.java
+++ advanced-jaxrs-01/src/main/java/org/orhan/javabrains/org/orhan/javabrains/client/RestApiClient.java
@@ -1,10 +1,20 @@
 package org.orhan.javabrains.client;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.Response;
+
+import org.orhan.messenger.model.Message;
+
+
 public class RestApiClient {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("deneme");
+		Client client = ClientBuilder.newClient();
+		Response response=client.target("http://localhost:8080/advanced-jaxrs-01/webapi/messages/1").request().get();
+		Message message=response.readEntity(Message.class);
+		System.out.println(message.getMessage());
+		
 	}
 
 }
---
+++ advanced-jaxrs-01/src/org/orhan/messenger/resources/CommentResource.java
@@ -0,0 +1,19 @@
+package org.orhan.messenger.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("/")
+public class CommentResource {
+	@GET 
+	public String test() {
+		return "new sub resource";
+	}
+	
+	@GET
+	@Path("/{commentId}")
+	public String test2(@PathParam("messageId") long messageId,@PathParam("commentId") long commentId) {
+		return "method to return commentID:"+commentId+", messageid:"+messageId;
+	}
+}
